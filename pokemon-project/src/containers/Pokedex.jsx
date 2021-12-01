f0de39578d0239889dbd1b709d3b9c7bdb389665
T U <tylerletendre98@gmail.com>
2021-11-30 21:24:18 -0500

    added in the api and retrieved the information from the api

--- pokemon-project/src/containers/Pokedex.jsx
+++ pokemon-project/src/containers/Pokedex.jsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { Box } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { Box, CircularProgress } from "@material-ui/core";
+import axios from "axios";
+import { POKEMON_API_URL, IMAGE_API_URL } from "../config";
 
 const Pokedex = () => {
-  useEffect(() => {}, []);
+  const [pokemonData, setpokemonData] = useState([]);
+  useEffect(() => {
+    axios.get(`${POKEMON_API_URL}?limit=800`).then((res) => {
+      if (res.status >= 200 && res.status < 3000) {
+        const { results } = res.data;
+        let newPokemonData = [];
+        results.forEach((pokemon, index) => {
+          index++;
+          let pokemonObject = {
+            id: index,
+            url: IMAGE_API_URL + index + ".png",
+            name: pokemon.name,
+          };
+          newPokemonData.push(pokemonObject);
+        });
+        setpokemonData(newPokemonData);
+      }
+    });
+  }, []);
 
   return (
     <Box>
-      <h1>something</h1>
+      {pokemonData ? (
+        pokemonData.map((pokemon) => {
+          return <h1>{pokemon.name}</h1>;
+        })
+      ) : (
+        <CircularProgress />
+      )}
     </Box>
   );
 };
