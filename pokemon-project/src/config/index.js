f1321997317d0447c834d50556d27c354300cf48
T U <tylerletendre98@gmail.com>
2021-12-02 12:06:54 -0500

    adding in the axios call to get the detail information for the pokemon

--- pokemon-project/src/config/index.js
+++ pokemon-project/src/config/index.js
@@ -1,2 +1,6 @@
+
+
 export const POKEMON_API_URL = 'https://pokeapi.co/api/v2/pokemon'
 export const IMAGE_API_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+
+export default POKEMON_API_URL;
--- pokemon-project/src/containers/PokemonData.jsx
+++ pokemon-project/src/containers/PokemonData.jsx
@@ -1,6 +1,27 @@
+import axios from 'axios'
 import React, { Component } from 'react'
+import POKEMON_API_URL from '../config/index'
 
 export default class PokemonData extends Component {
+constructor(props){
+    super(props)
+        this.state={
+            pokemon: null
+        
+    }
+}
+
+componentDidMount(){
+    const { match } = this.props;
+    const {id} = match?.parmas;
+    axios.get(`${POKEMON_API_URL}/${id}`).then((res)=>{
+        if (res.status>=200 && res.status < 300){
+            console.log(res.data)
+            this.setState({pokemon: res.data})
+        }
+    })
+}
+
     render() {
         return (
             <div>
